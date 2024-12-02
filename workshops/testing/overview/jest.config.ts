3850e32223a04062452e269d0c7a475218693c12
pablocru <pablocru@outlook.com>
2024-12-02 20:14:53 +0100

    chore: Disable `Coverage` test
    As the source code comes from an external package, `Coverage` test is no longer needed

--- workshops/testing/overview/jest.config.ts
+++ workshops/testing/overview/jest.config.ts
@@ -6,16 +6,6 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  // Indicates whether the coverage information should be collected while executing the
-  // test
-  collectCoverage: true,
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
-
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
-
   // A preset that is used as a base for Jest's configuration
   preset: "ts-jest",
 
