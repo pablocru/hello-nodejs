74d753b129112d198594aa60499dba3b45a5fd0c
pablocru <pablocru@outlook.com>
2025-08-05 18:51:39 +0200

    feat: Send a simple HTML with status code instead of a plain text

--- workshops/web-server/rest-web/src/app.ts
+++ workshops/web-server/rest-web/src/app.ts
@@ -5,7 +5,8 @@ const greet = "Hello, World!";
 const server = http.createServer((request, response) => {
   console.log(request.url);
 
-  response.write(greet);
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(`<h1>${greet}</h1>`);
   response.end();
 });
 
