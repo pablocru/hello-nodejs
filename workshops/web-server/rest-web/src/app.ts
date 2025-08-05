614fb9f09f23966d508a6bc2ff0d8273bc9eae97
pablocru <pablocru@outlook.com>
2025-08-05 18:44:57 +0200

    feat: Create a simple Rest Server that displays a greeting

--- workshops/web-server/rest-web/src/app.ts
+++ workshops/web-server/rest-web/src/app.ts
@@ -1,3 +1,14 @@
+import http from "http";
+
 const greet = "Hello, World!";
 
-console.log({ greet });
+const server = http.createServer((request, response) => {
+  console.log(request.url);
+
+  response.write(greet);
+  response.end();
+});
+
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
