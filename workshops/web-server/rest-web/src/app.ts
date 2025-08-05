e129eb14b891aa0e5a1eb739b9054d98968bb1ec
pablocru <pablocru@outlook.com>
2025-08-05 19:27:54 +0200

    feat: Send CSS and JS files

--- workshops/web-server/rest-web/src/app.ts
+++ workshops/web-server/rest-web/src/app.ts
@@ -2,20 +2,46 @@ import fs from "fs";
 import http from "http";
 
 const htmlContentType = { "Content-Type": "text/html" };
+const cssContentType = { "Content-Type": "text/css" };
+const jsContentType = { "Content-Type": "application/javascript" };
 
 const server = http.createServer((request, response) => {
   console.log(request.url);
 
   switch (request.url) {
     case "/": {
-      fs.readFile("./public/index.html", (err, htmlFile) => {
+      fs.readFile("./public/index.html", (err, readFile) => {
         if (err) {
           response
             .writeHead(500, htmlContentType)
             .end("<h1>Internal Server Error</h1>");
           return;
         }
-        response.writeHead(200, htmlContentType).end(htmlFile);
+        response.writeHead(200, htmlContentType).end(readFile);
+      });
+      break;
+    }
+    case "/style/main.css": {
+      fs.readFile("./public/style/main.css", (err, readFile) => {
+        if (err) {
+          response
+            .writeHead(500, htmlContentType)
+            .end("<h1>Internal Server Error</h1>");
+          return;
+        }
+        response.writeHead(200, cssContentType).end(readFile);
+      });
+      break;
+    }
+    case "/js/main.js": {
+      fs.readFile("./public/js/main.js", (err, readFile) => {
+        if (err) {
+          response
+            .writeHead(500, htmlContentType)
+            .end("<h1>Internal Server Error</h1>");
+          return;
+        }
+        response.writeHead(200, jsContentType).end(readFile);
       });
       break;
     }
