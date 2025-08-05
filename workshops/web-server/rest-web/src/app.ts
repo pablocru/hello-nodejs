2917bd4a0fc4e985838e0b74decf7da459270f39
pablocru <pablocru@outlook.com>
2025-08-05 19:11:13 +0200

    feat: Send the HTML file only if the request URL is the home page, and return a "Not Found" status code otherwise

--- workshops/web-server/rest-web/src/app.ts
+++ workshops/web-server/rest-web/src/app.ts
@@ -1,12 +1,20 @@
 import fs from "fs";
 import http from "http";
 
+const htmlContentType = { "Content-Type": "text/html" };
+
 const server = http.createServer((request, response) => {
   console.log(request.url);
 
-  const htmlFile = fs.readFileSync("./public/index.html");
-
-  response.writeHead(200, { "Content-Type": "text/html" }).end(htmlFile);
+  switch (request.url) {
+    case "/": {
+      const htmlFile = fs.readFileSync("./public/index.html");
+      response.writeHead(200, htmlContentType).end(htmlFile);
+      break;
+    }
+    default:
+      response.writeHead(404, htmlContentType).end();
+  }
 });
 
 server.listen(8080, () => {
